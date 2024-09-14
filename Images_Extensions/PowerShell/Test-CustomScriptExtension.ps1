74d1e48a9f4f903380a53123f19c60e9572e1ecd
Craig Landis <clandis@microsoft.com>
2024-09-13 23:51:39 -0700

    Update 2024-09-13T23:51:39

--- Images_Extensions/PowerShell/Test-CustomScriptExtension.ps1
+++
@@ -1,5 +0,0 @@
-Start-Transcript -Path "$env:SystemRoot\Temp\PowerShell_transcript.$($env:COMPUTERNAME).$(Get-Date ((Get-Date).ToUniversalTime()) -f yyyyMMddHHmmss).txt" -IncludeInvocationHeader
-Set-PSDebug -Trace 2
-Write-Output "Write-Output output from $($MyInvocation.MyCommand.Name)"
-exit 2
-Stop-Transcript
