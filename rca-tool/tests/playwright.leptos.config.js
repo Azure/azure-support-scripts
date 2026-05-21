555c754bfc33029a5b6d9bba821cb4b9c41b4ab4
Alvaro Figueroa <alvaro.figueroa@microsoft.com>
2026-05-21 13:25:20 -0600

    No retry in playwright to make it fail faster

--- rca-tool/tests/playwright.leptos.config.js
+++ rca-tool/tests/playwright.leptos.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   testDir: './',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,
   workers: process.env.CI ? 1 : 4,
   reporter: [
     ['html', { outputFolder: 'playwright-report-leptos', open: 'never' }],
